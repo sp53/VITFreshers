32d70f5361ca6f4a2b6796920baa03f767ce6642
Vaibhav Mishra <vaibhavmishra740@gmail.com>
2019-06-09 21:06:51 +0530

    Login Page Added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText departement = findViewById(R.id.editText2);
+
+        departement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("CHK", context.toString());
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.custom_layout);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                dialog.show();
+
+                TextView cse = dialog.findViewById(R.id.cse);
+                cse.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        departement.setText("CSE");
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
     }
 }
