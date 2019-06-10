ebaa0dc4d1091d70cd6655003abf598a7c6c7ed6
sp53 <sparshbansal53@gmail.com>
2019-06-10 12:00:07 +0530

    Splash Screen Added and minor constraints setup in login screen

---
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    private static int Splash_Time = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, Splash_Time);
+
+    }
+
+
+
+
+}
