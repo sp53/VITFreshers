79c1448e4cf3992132fdd1ccf7ad043e0bb0120c
sp53 <sparshbansal53@gmail.com>
2019-06-11 18:12:58 +0530

    Tabbed layout added in ScheduleActivity for separate college and department schedules.

--- app/src/main/java/com/example/myapplication/ScheduleActivity.java
+++ app/src/main/java/com/example/myapplication/ScheduleActivity.java
@@ -7,12 +7,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
 public class ScheduleActivity extends AppCompatActivity {
-    private ArrayList<Schedule> scheduleArrayList;
-    private ListView listView;
+    SectionPageAdapter adapter;
+    SectionPageAdapter spa;
+    ViewPager mviewpager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,15 +29,41 @@ public class ScheduleActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("VIT Freshers");
 
-        scheduleArrayList = new ArrayList<>();
 
-        // For Testing
-        for (int i = 0; i < 10; i++) {
-            scheduleArrayList.add(new Schedule());
-        }
+        adapter=new SectionPageAdapter(getSupportFragmentManager());
+        spa=new SectionPageAdapter(getSupportFragmentManager());
+        mviewpager=(ViewPager) findViewById(R.id.container);
 
-        listView = findViewById(R.id.schedule_listview);
-        MyListAdapter myListAdapter = new MyListAdapter(this, scheduleArrayList);
-        listView.setAdapter(myListAdapter);
+        mviewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                FragmentSwipeInterface fragment = (FragmentSwipeInterface) adapter.instantiateItem(mviewpager, i);
+                if (fragment != null) {
+                    fragment.fragmentBecameVisible();
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+
+        });
+
+        setupViewPager(mviewpager);
+        TabLayout tbl=(TabLayout) findViewById(R.id.tabs);
+        tbl.setupWithViewPager(mviewpager);
+    }
+
+    private void setupViewPager(ViewPager vp)
+    {
+        adapter.addFragment(new collegefrag(),"College");
+        adapter.addFragment(new departmentfrag(),"Department");
+        vp.setAdapter(adapter);
     }
 }
