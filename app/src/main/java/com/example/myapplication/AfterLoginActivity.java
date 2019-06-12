621ba9bad610f13af94218d30201b1ce292e2117
sp53 <sparshbansal53@gmail.com>
2019-06-12 12:42:15 +0530

    Button Visibility Reverted.

--- app/src/main/java/com/example/myapplication/AfterLoginActivity.java
+++ app/src/main/java/com/example/myapplication/AfterLoginActivity.java
@@ -41,27 +41,10 @@ public class AfterLoginActivity extends AppCompatActivity {
 
     public void show_proctor_name(View view) {
 
-        final Button b=findViewById(R.id.button);
-        final Button b2=findViewById(R.id.button2);
-        final Button b4=findViewById(R.id.button4);
-
-        b.setVisibility(View.INVISIBLE);
-        b2.setVisibility(View.INVISIBLE);
-        b4.setVisibility(View.INVISIBLE);
-
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.proctor_dialog_layout);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialogInterface) {
-                b.setVisibility(View.VISIBLE);
-                b2.setVisibility(View.VISIBLE);
-                b4.setVisibility(View.VISIBLE);
-
-            }
-        });
         dialog.show();
     }
 }
