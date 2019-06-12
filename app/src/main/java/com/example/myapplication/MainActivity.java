5f37480883beef098651ce43038ffeab9e37da36
Vaibhav Mishra <vaibhavmishra740@gmail.com>
2019-06-12 14:06:08 +0530

    Added more branches

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,9 +12,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText departement;
+    private EditText regNo;
     final Context context = this;
 
     @Override
@@ -22,14 +25,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText departement = findViewById(R.id.editText2);
-        final EditText regNo = findViewById(R.id.editText);
+        departement = findViewById(R.id.editText2);
+        regNo = findViewById(R.id.editText);
 
 
         departement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("CHK", context.toString());
+                //Log.d("CHK", context.toString());
 
                 regNo.clearFocus();
 
@@ -41,6 +44,12 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
 
                 TextView cse = dialog.findViewById(R.id.cse);
+                TextView eee = dialog.findViewById(R.id.eee);
+                TextView ece = dialog.findViewById(R.id.ece);
+                TextView mechanical = dialog.findViewById(R.id.mech);
+                TextView ecm = dialog.findViewById(R.id.ecm);
+                TextView civil = dialog.findViewById(R.id.civil);
+
                 cse.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -48,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 });
-                TextView eee = dialog.findViewById(R.id.eee);
+
                 eee.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -56,11 +65,35 @@ public class MainActivity extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 });
-                TextView ece = dialog.findViewById(R.id.ece);
+
                 ece.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         departement.setText("ECE");
+                        dialog.dismiss();
+                    }
+                });
+
+                ecm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        departement.setText("ECM");
+                        dialog.dismiss();
+                    }
+                });
+
+                civil.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        departement.setText("CIVIL");
+                        dialog.dismiss();
+                    }
+                });
+
+                mechanical.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        departement.setText("MECHANICAL");
                         dialog.dismiss();
                     }
                 });
@@ -69,8 +102,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
+        String regno = regNo.getText().toString().trim();
+        String dep = departement.getText().toString().trim();
+
+        if(regno.isEmpty() || dep .isEmpty()) {
+            Toast.makeText(this, "Fill all fields", Toast.LENGTH_LONG).show();
+            return;
+        }
         Intent intent = new Intent(this, AfterLoginActivity.class);
         startActivity(intent);
-        Log.d("CLICKED", "TRUE");
+        //Log.d("CLICKED", "TRUE");
     }
 }
