670236f79fc4f5fd175ab267e7ba5bcad5e55b52
sp53 <sparshbansal53@gmail.com>
2019-07-03 23:41:34 +0530

    Proctor Details connected with database.

--- app/src/main/java/com/example/myapplication/AfterLoginActivity.java
+++ app/src/main/java/com/example/myapplication/AfterLoginActivity.java
@@ -7,14 +7,36 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class AfterLoginActivity extends AppCompatActivity {
+
+    String ProctorName="";
+    String ProctorRoom="";
+
+    String JSON_STRING;
+    String jsdata;
+    JSONObject jsobject;
+    JSONArray jsarray;
+    String indata="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +54,8 @@ public class AfterLoginActivity extends AppCompatActivity {
             finish();
         }
 
+        getJSON();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.bringToFront();
@@ -42,6 +66,7 @@ public class AfterLoginActivity extends AppCompatActivity {
 
         TextView tv = findViewById(R.id.textView);
         tv.setText("Hello, "+SaveSharedPreference.getUserName());
+
 
     }
 
@@ -64,7 +89,14 @@ public class AfterLoginActivity extends AppCompatActivity {
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
 
+        TextView tv1=dialog.findViewById(R.id.procName);
+        tv1.setText("Name : "+ProctorName);
+        TextView tv2=dialog.findViewById(R.id.procRoom);
+        tv2.setText("Room No. : "+ProctorRoom);
+
         dialog.show();
+
+
     }
 
     @Override
@@ -99,5 +131,79 @@ public class AfterLoginActivity extends AppCompatActivity {
         startActivity(intent);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         finish();
+    }
+
+    // json data
+    public void getJSON()
+    {
+
+        new BackgroundTask().execute();
+
+    }
+    class BackgroundTask extends AsyncTask<Void,Void,String>
+    {
+        String json_url;
+        @Override
+        protected void onPreExecute() {
+
+            String u="https://vitappapi.herokuapp.com/?reg="+SaveSharedPreference.getUserName();
+            json_url=u;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+                URL url= new URL(json_url);
+                HttpURLConnection httpURLConnection;
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader br =new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                StringBuilder sb=new StringBuilder();
+                while((JSON_STRING=br.readLine())!=null)
+                {
+                    sb.append(JSON_STRING+"\n");
+                }
+                br.close();
+                httpURLConnection.disconnect();
+                return sb.toString().trim();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            indata=result;
+
+
+            try {
+                jsobject = new JSONObject(indata);
+                JSONObject jo;
+
+
+                jsarray = jsobject.getJSONArray("MAIN_ACTIVITY");
+                jo = jsarray.getJSONObject(0);
+                ProctorName = jo.getString("Proctor");
+                ProctorRoom = jo.getString("ProcVenue");
+
+
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 }
