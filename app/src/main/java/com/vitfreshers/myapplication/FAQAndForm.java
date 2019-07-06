a2ba42d8260419619730aedfdc049ceed24a76e1
sp53 <sparshbansal53@gmail.com>
2019-07-06 14:10:54 +0530

    Webview added for FAQs.

--- app/src/main/java/com/vitfreshers/myapplication/FAQAndForm.java
+++ app/src/main/java/com/vitfreshers/myapplication/FAQAndForm.java
@@ -6,8 +6,17 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class FAQAndForm extends AppCompatActivity {
+
+    WebView disp;
+    ProgressBar pbr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,37 @@ public class FAQAndForm extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("VIT Freshers");
+
+        pbr = findViewById(R.id.progressBar2);
+
+        disp = findViewById(R.id.webView);
+        disp.getSettings().setJavaScriptEnabled(true);
+        disp.loadUrl("http://vitappapi.herokuapp.com/faqffcs.php");
+
+        disp.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                if (progress == 100) {
+                    pbr.setVisibility(View.GONE);
+
+                } else {
+                    pbr.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        //disp.loadUrl("https://forms.gle/bYTuXW9TdMnngypz9");
+    }
+
+    public void open_hostel_faqs(View view)
+    {
+        String pdf = "https://plasma2k19.000webhostapp.com/vitappapi/hostelfaq.pdf";
+        disp.loadUrl("https://docs.google.com/viewer?url=" + pdf);
+        //disp.loadUrl("http://vitappapi.herokuapp.com/faqhostel.php");
+
+    }
+
+    public void open_ffcs_faqs(View view)
+    {
+        disp.loadUrl("http://vitappapi.herokuapp.com/faqffcs.php");
     }
 
     @Override
